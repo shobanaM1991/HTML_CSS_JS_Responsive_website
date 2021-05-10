c06804b5aa35e23f44862a61106711acdcbbe041
shobanaM1991 <shobana.mariappan1991@gmail.com>
2021-05-10 10:55:36 +0200

    href added in contactUs

--- Ecommerce-web_V1.0/src/components/header_components/ContactUs.js
+++ Ecommerce-web_V1.0/src/components/header_components/ContactUs.js
@@ -13,7 +13,7 @@ function Contacts(){
                             <path d="M497.39 361.8l-112-48a24 24 0 0 0-28 6.9l-49.6 60.6A370.66 370.66 0 0 1 130.6 204.11l60.6-49.6a23.94 23.94 0 0 0 6.9-28l-48-112A24.16 24.16 0 0 0 122.6.61l-104 24A24 24 0 0 0 0 48c0 256.5 207.9 464 464 464a24 24 0 0 0 23.4-18.6l24-104a24.29 24.29 0 0 0-14.01-27.6z">
                             </path>
                         </svg>
-                        <a>015229962032</a>
+                        <a href="#" alt ="">015229962032</a>
                     </div>
                     <div className="email-icon">
                         <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 512 512" class="contact-module--email" height="1.2em" width="1.2em" xmlns="http://www.w3.org/2000/svg">
