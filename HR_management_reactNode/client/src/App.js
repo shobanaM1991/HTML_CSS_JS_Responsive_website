7f9ec67f44c04f03ab8810830eef637dc2824daa
shobanaM1991 <shobana.mariappan1991@gmail.com>
2021-06-23 11:59:15 +0200

    Nodejs and Mysql included with CRUD Functions

--- HR_management_reactNode/client/src/App.js
+++ HR_management_reactNode/client/src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import { useState } from "react"
+import Axios from 'axios'
+//import { response } from 'express';
 
 function App() {
 
@@ -9,6 +11,32 @@ function App() {
   const [position, setPosition]=useState(" ");
   const [wage, setWage]=useState(0);
 
+  const[employeeList, setEmployeeList] = useState([]);
+
+  
+  const addEmployee = () => {
+    Axios.post('http://localhost:3001/create', {
+      name:name, 
+      age:age, 
+      country:country,
+      position:position,
+      wage:wage,
+    }).then(()=>{
+      setEmployeeList([...employeeList, {
+        name:name, 
+        age:age, 
+        country:country,
+        position:position,
+        wage:wage,
+      }]);
+    });
+  };
+
+  const getEmployee = () => {
+    Axios.get('http://localhost:3001/employees').then((response)=>{
+      setEmployeeList(response.data);
+    });
+  };
 
   return (
     <div className="App">
@@ -48,9 +76,32 @@ function App() {
             setWage(event.target.value)
           }}
         />
-        <button >Add Employee</button>
+        <button onClick = {addEmployee}>Add Employee</button>
       </div>
+      <hr
+        style={{
+            margin:15,
+            color: 'blue',
+            backgroundColor: 'blue',
+            width: 1890
+        }}
+    />
+    <div className="employees">
+      <button onClick={getEmployee}>Show Employee</button>
+      {employeeList.map((val, index) => {
+        return (
+        <div className="employee">
+          <h3>Name: {val.name}</h3>
+          <h3>Age: {val.age}</h3>
+          <h3>Position: {val.position}</h3>
+          <h3>Country: {val.country}</h3>
+          <h3>Wage: {val.wage}</h3>
+        </div>
+        );
+      })}
     </div>
+    
+  </div>
   );
 }
 
